b85111dcedbe57a75a0dde56e3a97b84ea617a13
Manoj-K-Sarkar <wakeupmanoj123@gmail.com>
2020-04-13 11:03:12 +0530

    Add Environmental Variable using dotenv

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
 
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -24,9 +25,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "Thisisasimplesecret.";
-
-userSchema.plugin(encrypt, {secret: secret, encryptedFields:["password"]});
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields:["password"]});
 
 const User = mongoose.model("USer", userSchema);
 
@@ -69,10 +68,8 @@ app.post("/login", function(req, res) {
             console.log(err);
         else {
             if(foundUser) {
-                if(foundUser.password === password){
-                    console.log("In foundUSerPassword!");
+                if(foundUser.password === password)
                     res.render("secrets");
-                }
             }
         }
     });
